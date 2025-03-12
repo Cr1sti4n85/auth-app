fd749171e7d464ee40f7b85bb02ba0f729c0d91f
Cristian Perez <cperezlecaros@gmail.com>
2025-03-12 13:20:29 -0700

    Create status codes constants

--- backend/src/middleware/errorHandler.ts
+++ backend/src/middleware/errorHandler.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction, ErrorRequestHandler } from "express";
+import { INTERNAL_SERVER_ERROR } from "../config/statusCodes";
 
 const errorHandler: ErrorRequestHandler = (
   err: Error,
@@ -7,7 +8,7 @@ const errorHandler: ErrorRequestHandler = (
   next: NextFunction
 ) => {
   console.log(`PATH: ${req.path}`, err);
-  res.status(500).send("Internal Server Error");
+  res.status(INTERNAL_SERVER_ERROR).send("Internal Server Error");
 };
 
 export default errorHandler;
