c5d36cadbcd4848f1de5b62d2c026d3eb4a64e33
Cristian Perez <cperezlecaros@gmail.com>
2025-03-14 13:45:57 -0700

    Add zod error handler

--- backend/src/middleware/errorHandler.ts
+++ backend/src/middleware/errorHandler.ts
@@ -1,5 +1,20 @@
 import { Request, Response, NextFunction, ErrorRequestHandler } from "express";
-import { INTERNAL_SERVER_ERROR } from "../config/statusCodes";
+import { BAD_REQUEST, INTERNAL_SERVER_ERROR } from "../config/statusCodes";
+import { z } from "zod";
+
+const handleZodError = (res: Response, error: z.ZodError) => {
+  const errors = error.issues.map((err) => {
+    return {
+      path: err.path.join("."),
+      message: err.message,
+    };
+  });
+
+  res.status(BAD_REQUEST).json({
+    message: error.message,
+    errors,
+  });
+};
 
 const errorHandler: ErrorRequestHandler = (
   err: Error,
@@ -7,6 +22,9 @@ const errorHandler: ErrorRequestHandler = (
   res: Response,
   next: NextFunction
 ) => {
+  if (err instanceof z.ZodError) {
+    return handleZodError(res, err);
+  }
   console.log(`PATH: ${req.path}`, err);
   res.status(INTERNAL_SERVER_ERROR).send("Internal Server Error");
 };
