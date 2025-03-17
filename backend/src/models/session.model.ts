129f22e42918736fddcb0a6ca081e41ea7422e28
Cristian Perez <cperezlecaros@gmail.com>
2025-03-17 10:35:19 -0700

    Create session model

--- backend/src/models/session.model.ts
+++ backend/src/models/session.model.ts
@@ -1,1 +1,34 @@
 import mongoose, { Schema } from "mongoose";
+import { Session } from "../types/session.types";
+import { thirtyDaysFromNow } from "../lib/date";
+
+const SessionSchema: Schema = new Schema<Session>(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      index: true,
+      ref: "User",
+      required: true,
+    },
+
+    userAgent: {
+      type: String,
+    },
+    expiresAt: {
+      type: Date,
+      default: thirtyDaysFromNow,
+    },
+    createdAt: {
+      type: Date,
+      required: true,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const SessionModel = mongoose.model<Session>("Session", SessionSchema);
+
+export default SessionModel;
