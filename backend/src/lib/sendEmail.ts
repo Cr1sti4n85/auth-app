4dda1fdc4dc35b1982b00cdc65519dc467c7dea0
Cristian Perez <cperezlecaros@gmail.com>
2025-03-24 21:56:05 -0700

    Update sendEmail

--- backend/src/lib/sendEmail.ts
+++ backend/src/lib/sendEmail.ts
@@ -1,10 +1,17 @@
+import { EMAIL_SENDER, NODE_ENV } from "../config/envConfig";
 import resend from "../config/resend";
 import { EmailParams } from "../types/email.types";
 
+const senderEmail =
+  NODE_ENV === "development" ? "onboarding@resend.dev" : EMAIL_SENDER;
+
+const toEmail = (to: string) =>
+  NODE_ENV === "development" ? "delivered@resend.dev" : to;
+
 export const sendEmail = async ({ to, subject, text, html }: EmailParams) =>
   await resend.emails.send({
-    from: "onboarding@resend.dev",
-    to: "delivered@resend.dev",
+    from: senderEmail,
+    to: toEmail(to),
     subject,
     text,
     html,
